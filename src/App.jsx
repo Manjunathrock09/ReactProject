7d423453d3d1cbf39f1518bfd7ff26d28f9a124f
Manjunathrock09 <manjunathsai789@gmail.com>
2025-03-07 18:15:47 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import Header from './components/Header';
+import React  from 'react';
+import Header from './components/Header'; 
 import Hero from './components/Hero';
 import Features from './components/Features';
 import Footer from './components/Footer';
