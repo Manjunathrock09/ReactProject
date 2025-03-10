0f88e2cbb900b5c5b54870eaf76b05e6aaff7c8b
Manjunathrock09 <manjunathsai789@gmail.com>
2025-03-10 18:43:48 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "antd/dist/reset.css"; // Ant Design's latest CSS reset for better styling
-import "./styles/index.css"; // Custom styles
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
